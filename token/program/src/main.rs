28c8f5429ef50e980441a474ced879e72c9e2ed4
Bruno Ochotorena <hackerhilt@gmail.com>
2022-12-22 18:56:39 -0300

    feat: add stub via library

--- token/program/src/main.rs
+++ token/program/src/main.rs
@@ -1,10 +1,10 @@
 pub(crate) use std::io;
-use spl_token::{error::TokenError, processor::Processor};
-use cartesi_solana::adapter::{get_processor_args, persist_accounts};
-use solana_program::{pubkey::Pubkey, account_info::AccountInfo, program_error::PrintProgramError};
-use cartesi_solana::anchor_lang::solana_program::entrypoint::ProgramResult;
+use spl_token::{processor::Processor};
+use cartesi_solana::{adapter::{get_processor_args, persist_accounts}, cartesi_stub::CartesiStubs};
 
 fn main() -> io::Result<()> {
+    solana_program::program_stubs::set_syscall_stubs(Box::new(CartesiStubs {}));
+
     let (program_id, accounts, data, last_instruction) = get_processor_args();
     Processor::process(&program_id, &accounts, &data).unwrap();
     persist_accounts(&accounts, last_instruction);
